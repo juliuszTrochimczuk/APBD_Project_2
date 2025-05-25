0b41c5f2c44aa5afc3d9088ed150ddd72d8d9bca
Juliusz Trochimczuk <assasindal@gmail.com>
2025-05-25 13:40:15 +0200

    Creating models based on sql + prepping to write endpoints

---
+++ src/API/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? 
+                          throw new InvalidOperationException("There is no default connection string");
+
+builder.Services.AddDbContext<MasterContext>(option => option.UseSqlServer(connectionString));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.Run();
